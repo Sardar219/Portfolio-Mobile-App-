1c5e4673b55c734c3172c5e5d8a4c2f0c3f93c59
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-03 13:46:21 +0430

    made flutter project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+import "package:flutter_first/home.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "googlesans"),
+      home: Home(),
+      routes: {},
+    );
+  }
+}
