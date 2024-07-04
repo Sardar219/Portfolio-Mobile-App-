b672fc0de627e6743b637861f11a394944b5a797
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-04 17:17:12 +0430

    add appbar

--- lib/home.dart
+++ lib/home.dart
@@ -6,9 +6,25 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text(
+            "Sardar Hossaini",
+            style: TextStyle(fontSize: 18),
+          ),
+          centerTitle: true,
+          actions: [
+            Container(
+              margin: EdgeInsets.only(right: 15),
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/ali.HEIC"),
+              ),
+            ),
+          ],
+          elevation: 2,
+          shadowColor: Colors.blueGrey,
+          foregroundColor: Colors.white,
+        ),
+      );
   }
 }
