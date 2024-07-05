6917d3154da8e58912e5d776879a18b5bb038aa3
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-05 08:50:31 +0430

    add contact info in contact page

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_first/routes/drawer.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Contact extends StatelessWidget {
   const Contact({super.key});
@@ -27,7 +28,112 @@ class Contact extends StatelessWidget {
         foregroundColor: Colors.white,
       ),
       drawer: NavBar(),
-      body: Center(child: Text("Contact"),),
+      body: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    width: 230,
+                    height: 230,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/logo.HEIC"),
+                    )),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "Sardar Hossaini",
+                    style: TextStyle(fontSize: 22),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: FaIcon(FontAwesomeIcons.facebook),
+                      color: Colors.blueAccent,
+                    ),
+                    IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.instagram,
+                      ),
+                      onPressed: () {},
+                      color: Colors.blueAccent,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: FaIcon(FontAwesomeIcons.twitter),
+                      color: Colors.blueAccent,
+                    ),
+                    IconButton(
+                      icon: FaIcon(FontAwesomeIcons.linkedinIn),
+                      onPressed: () {},
+                      color: Colors.blueAccent,
+                    ),
+                  ],
+                ),
+                Center(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.mail,
+                            size: 20,
+                          ),
+                          color: Colors.blueAccent,
+                          hoverColor: Colors.grey[300],
+                        ),
+                        Text(
+                          "sardarhossaini2022@gmail.com",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ]),
+                ),
+                Center(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.phone,
+                            size: 20,
+                          ),
+                          color: Colors.blueAccent,
+                          hoverColor: Colors.grey[300],
+                        ),
+                        Text("+93 77 402 7496"),
+                      ]),
+                ),
+                Center(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.location_on,
+                            size: 20,
+                          ),
+                          color: Colors.blueAccent,
+                          hoverColor: Colors.grey[300],
+                        ),
+                        Text("Kabul, Afghanistan"),
+                      ]),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
